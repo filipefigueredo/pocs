4a229e3ca91579b2a2b3daca9a1bdb1745330ea4
Filipe Figueredo Silva <filipe.figueredo.silva@gmail.com>
2022-03-23 20:22:24 +0000

    Update main.tf

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -7,8 +7,6 @@ resource "azurerm_resource_group" "rg" {
 locals {
   test1 = "This is my output 01"
   test2 = "This is my output 02"
-  
-  test3 = "This is my output 03"
 }
 
 output "first_output" {
@@ -17,9 +15,5 @@ output "first_output" {
 
 output "second_output" {
   value = local.test2
-}
-
-output "third_output" {
-  value = local.test3
 }
 
